a15b341d8b96a36f3bf81a76afe16f8bf3f144a6
BigDataIsVeryImportantInAI <znfks8819@gmail.com>
2017-05-24 15:48:17 +0900

    Finished chapter 13 but not done challenge and more

--- app/src/main/java/kr/forsee/criminalintent/CrimeFragment.java
+++ app/src/main/java/kr/forsee/criminalintent/CrimeFragment.java
@@ -1,5 +1,7 @@
 package kr.forsee.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -26,6 +28,8 @@ public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "cirme_id";
     private static final String DIALOG_DATE = "DialogDate";
+    private static final int REQUEST_DATE = 0;
+
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
@@ -66,14 +70,13 @@ public class CrimeFragment extends Fragment {
         });
 
         mDateButton = (Button) v.findViewById(R.id.crime_date);
-        CharSequence dataFormat = "EEE, MMM d, yyyy";
-        String date = DateFormat.format(dataFormat, mCrime.getDate()).toString();
-        mDateButton.setText(date);
+        updateDate();
         mDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentManager manager = getFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
                 dialog.show(manager, DIALOG_DATE);
             }
         });
@@ -88,5 +91,23 @@ public class CrimeFragment extends Fragment {
         });
 
         return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_DATE) {
+            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate();
+        }
+    }
+
+    private void updateDate() {
+        CharSequence dataFormat = "EEE, MMM d, yyyy";
+        String date = DateFormat.format(dataFormat, mCrime.getDate()).toString();
+        mDateButton.setText(date);
     }
 }
--- app/src/main/java/kr/forsee/criminalintent/CrimeLab.java
+++ app/src/main/java/kr/forsee/criminalintent/CrimeLab.java
@@ -17,12 +17,17 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("범죄 #" + i);
-            crime.setSolved(i % 2 == 0);
-            mCrimes.add(crime);
-        }
+        // addCrime()의 추가로 필요가 없음
+        //for (int i = 0; i < 100; i++) {
+        //    Crime crime = new Crime();
+        //    crime.setTitle("범죄 #" + i);
+        //    crime.setSolved(i % 2 == 0);
+        //    mCrimes.add(crime);
+        //}
+    }
+
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
     }
 
     public static CrimeLab get(Context context) {
